bebcb278ca0e5bd18a25bba7e0524a84d4acf9c1
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-02-17 23:07:31 +0700

    Соответствие PEP8

--- Case.py
+++ Case.py
@@ -1,7 +1,8 @@
 # Case-study #1
-# Developer: Gerashchenko A.
+# Developers: Gerashchenko A, Matuzkov A.
 
 import turtle
+
 
 def square(x, y, a, color):
     '''
@@ -28,7 +29,8 @@ def square(x, y, a, color):
     turtle.right(90)
     turtle.end_fill()
 
-def triangle (x,y,a, color):
+
+def triangle(x, y, a, color):
     '''
     Function to draw a triangle
     :param x: the vertex of the triangle x coordinate
@@ -39,7 +41,7 @@ def triangle (x,y,a, color):
     '''
     turtle.up()
     turtle.setheading(0)
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.color(color)
     turtle.down()
     turtle.begin_fill()
@@ -51,7 +53,8 @@ def triangle (x,y,a, color):
     turtle.right(120)
     turtle.end_fill()
 
-def triangle_2(x,y,a,b,color):
+
+def right_triangle(x, y, a, b, color):
     '''
     Function to draw a triangle
     :param x: the vertex of the triangle x coordinate
@@ -63,24 +66,25 @@ def triangle_2(x,y,a,b,color):
     '''
     turtle.up()
     turtle.setheading(0)
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.color(color)
     turtle.down()
     turtle.begin_fill()
     turtle.forward(a)
     turtle.left(90)
     turtle.forward(b)
-    turtle.goto(x,y)
+    turtle.goto(x, y)
     turtle.end_fill()
 
-def circle (x,y,r, color):
+
+def circle(x, y, r, color):
     '''
     Function to draw a circle
     :param x: center of the circle x coordinate
     :param y: center of the circle y coordinate
     :param r: the radius of the circle
     :param color: color of the circle
-    :return:None
+    :return: None
     '''
     turtle.up()
     turtle.setheading(0)
@@ -91,7 +95,8 @@ def circle (x,y,r, color):
     turtle.circle(r)
     turtle.end_fill()
 
-def rhomb(x,y,a, color):
+
+def rhomb(x, y, a, color):
     '''
     Function to draw a rhomb
     :param x: upper corner of the rhomb x coordinate
@@ -109,14 +114,15 @@ def rhomb(x,y,a, color):
     turtle.forward(a)
     turtle.right(60)
     turtle.forward(a)
-    turtle.right (120)
+    turtle.right(120)
     turtle.forward(a)
     turtle.right(60)
     turtle.forward(a)
     turtle.right(120)
     turtle.end_fill()
 
-def rectangle (x,y,a,b, color):
+
+def rectangle(x, y, a, b, color):
     '''
     Function to draw a rectangle
     :param x: upper left corner x coordinate
@@ -141,7 +147,8 @@ def rectangle (x,y,a,b, color):
     turtle.forward(b)
     turtle.end_fill()
 
-def parallelogram (x,y,a,b, color):
+
+def parallelogram(x, y, a, b, color):
     '''
     Function to draw a parallelogram
     :param x: upper left corner x coordinate
@@ -167,7 +174,8 @@ def parallelogram (x,y,a,b, color):
     turtle.right(120)
     turtle.end_fill()
 
-def parallelogram_2 (x,y,a,b, color):
+
+def parallelogram_with_slope(x, y, a, b, color):
     '''
     Function to draw a parallelogram
     :param x: upper left corner x coordinate
@@ -193,7 +201,8 @@ def parallelogram_2 (x,y,a,b, color):
     turtle.right(120)
     turtle.end_fill()
 
-def parallelogram_3 (x,y,a,b, color):
+
+def parallelogram_another_slope(x, y, a, b, color):
     '''
     Function to draw a parallelogram
     :param x: upper left corner x coordinate
@@ -218,53 +227,59 @@ def parallelogram_3 (x,y,a,b, color):
     turtle.forward(b)
     turtle.left(120)
     turtle.end_fill()
+
 
 def main():
     turtle.speed(0)
 
-    parallelogram(0,0,100,50, 'blue')
-    triangle(101,0, 50, 'green')
-    rectangle(85,125, 15,125, 'brown')
-    triangle_2(65,125,20, -20, 'orange')
-    rhomb(25,26, 15, 'black')
-    circle(40,-30,10, 'skyblue')
+    # Кораблик
+    parallelogram(0, 0, 100, 50, 'blue')
+    triangle(101, 0, 50, 'green')
+    rectangle(85, 125, 15, 125, 'brown')
+    right_triangle(65, 125, 20, -20, 'orange')
+    rhomb(25, 26, 15, 'black')
+    circle(40, -30, 10, 'skyblue')
     circle(75, -30, 10, 'yellow')
     circle(110, -30, 10, 'grey')
-    circle(25,11, 8, 'pink')
-    parallelogram(40,125,30,20, 'lightgrey')
+    circle(25, 11, 8, 'pink')
+    parallelogram(40, 125, 30, 20, 'lightgrey')
 
+    # Сова
     rectangle(-200, 0, 100, 50, 'black')
     circle(-175, -25, 6, 'yellow')
     circle(-125, -25, 6, 'yellow')
-    triangle_2(-200, 1, 20, 30, 'black')
-    triangle_2(-100, 1, -20, 30, 'black')
+    right_triangle(-200, 1, 20, 30, 'black')
+    right_triangle(-100, 1, -20, 30, 'black')
     rhomb(-150, -25, 10, 'orange')
     triangle(-200, -50, 100, 'grey')
-    parallelogram_3(-100, -100, 25, 35, 'lightgrey')
-    parallelogram_2(-200, -100, 25, 35, 'lightgrey')
+    parallelogram_another_slope(-100, -100, 25, 35, 'lightgrey')
+    parallelogram_with_slope(-200, -100, 25, 35, 'lightgrey')
 
+    # Домик
     square(200, 50, 100, 'sienna')
-    square(230,25,40 , 'skyblue')
-    triangle(300,51,-100,'crimson')
-    rectangle(275,126, 20, 30, 'black')
-    triangle(275,95, 20,'black')
-    circle(250, 75,7, 'skyblue')
-    rectangle(300,-40,20,10, 'brown')
-    square(300,-30,10,'brown')
-    parallelogram(301,50,25,15,'orange')
-    triangle_2(310,37,-10,13, 'grey')
+    square(230, 25, 40, 'skyblue')
+    triangle(300, 51, -100, 'crimson')
+    rectangle(275, 126, 20, 30, 'black')
+    triangle(275, 95, 20, 'black')
+    circle(250, 75, 7, 'skyblue')
+    rectangle(300, -40, 20, 10, 'brown')
+    square(300, -30, 10, 'brown')
+    parallelogram(301, 50, 25, 15, 'orange')
+    right_triangle(310, 37, -10, 13, 'grey')
 
-    rectangle(0,-200,120,40,'lightgrey')
-    triangle_2(-41,-240,40,40, 'grey')
-    parallelogram_2(120,-240,40,30,'darkblue')
-    triangle_2(30,-200,30,30,'blue')
-    triangle_2(30,-240,30,-30,'blue')
-    circle(20,-230,10, 'skyblue')
-    rhomb(100,-240,10,'black')
-    rhomb(110,-240,10,'black')
+    # Самолет
+    rectangle(0, -200, 120, 40, 'lightgrey')
+    right_triangle(-41, -240, 40, 40, 'grey')
+    parallelogram_with_slope(120, -240, 40, 30, 'darkblue')
+    right_triangle(30, -200, 30, 30, 'blue')
+    right_triangle(30, -240, 30, -30, 'blue')
+    circle(20, -230, 10, 'skyblue')
+    rhomb(100, -240, 10, 'black')
+    rhomb(110, -240, 10, 'black')
 
     turtle.hideturtle()
     turtle.done()
+
 
 if __name__ == "__main__":
     main()
